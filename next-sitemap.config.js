f74b0db5a798cde3472f9e412d3d403c81d61c06
jo kalenberg <team@krefeld2030.de>
2024-10-22 16:57:53 +0200

    new sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,29 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.est1373.de/', // Ihre echte Domain hier einsetzen
+    siteUrl: 'https://www.est1373.de',  // Ohne Slash am Ende!
     generateRobotsTxt: true,
+    additionalPaths: async (config) => {
+      const result = []
+      
+      // Ihre spezifischen Seiten
+      const pages = [
+        '/',
+        '/botope',
+        '/about',
+        '/ideen',
+        '/shop'
+      ]
+      
+      for (const page of pages) {
+        result.push({
+          loc: page,
+          changefreq: 'daily',
+          priority: 0.7,
+          lastmod: new Date().toISOString(),
+        })
+      }
+      
+      return result
+    },
+    exclude: ['/api/*']
   }
